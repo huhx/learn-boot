d9fcc3504358ecfca6a847aa2a3cb9793f3f780d
huhongxiang <huhongxiang@csii.com.cn>
2018-12-15 16:06:04 +0800

    init commit [huhx] add springboot common module learn

---
+++ spring-redis/src/main/java/com/linux/huhx/HuhxApplication.java
@@ -0,0 +1,14 @@
+package com.linux.huhx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuhxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HuhxApplication.class, args);
+  }
+
+}
+
