63ec8d73d8c0c19bcfeab48d1b9e0e6ea7850d6f
huhongxiang <huhongxiang@csii.com.cn>
2018-12-15 16:04:43 +0800

    add some common module for springboot

---
+++ spring-redis/src/main/java/com/linux/huhx/model/Book.java
@@ -0,0 +1,14 @@
+package com.linux.huhx.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Book {
+
+  private String isbn;
+  private String title;
+}
