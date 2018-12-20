f41166f9a940f31e1cf9fe1d0c0cc79da55facc2
huhx <gohuhx@163.com>
2018-12-20 15:57:16 +0800

    rename spring-security project

--- spring-security/src/main/java/com/linux/huhx/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/linux/huhx/SpringSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HuhxApplication {
+public class SpringSecurityApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(HuhxApplication.class, args);
+    SpringApplication.run(SpringSecurityApplication.class, args);
   }
 
 }
