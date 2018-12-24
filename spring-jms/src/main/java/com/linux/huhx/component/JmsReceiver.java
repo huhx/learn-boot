8d6ebaa959fb0e46d32b4416395427a400ef2090
huhongxiang <huhongxiang@csii.com.cn>
2018-12-24 22:25:17 +0800

    jms service

--- spring-jms/src/main/java/com/linux/huhx/component/JmsReceiver.java
+++ spring-jms/src/main/java/com/linux/huhx/component/JmsReceiver.java
@@ -3,13 +3,3 @@ package com.linux.huhx.component;
 import com.linux.huhx.model.UserEmail;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
-
-@Component
-public class JmsReceiver {
-
-  @JmsListener(destination = "mailbox", containerFactory = "myFactory")
-  public void receiveMessage(UserEmail userEmail) {
-    System.out.println("Received <" + userEmail + ">");
-  }
-
-}
